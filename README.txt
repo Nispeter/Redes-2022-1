61a7fd34a80edce459741ca11f83a3b720b5de2a
Nispeter <Nispeter2@gmail.com>
2022-05-31 12:49:19 -0400

    Arreglos + symenc
    Modificacion de server y client para trabajar en base a paths relativos al nombre.
    Se anexan avances de la incriptacion simetrica con AES y pycryptodomo

--- README.txt
+++ README.txt
@@ -1,1 +1,3 @@
 PA TODAS LAS COSAS DE REDES
+
+server_test_symenc.cpp y client_test_symenc.cpp no funcionan
--- client_test_symenc.py
+++ client_test_symenc.py
@@ -1,9 +1,12 @@
 import socket                       #programacion de sockets duh...
 import os                           #preguntas a sistema 
+import hashlib 
 import tqdm                         #barra de progreso 
 import argparse                     #parsear argumentos pal modo de incriptacion 
 import tkinter as tk                #cosas de gui
 from tkinter import filedialog      
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
 
 #Valores para incriptar, tamaño del buffer de entrada y de envio, separador de string para el envio 
 FORM = "utf-8"
@@ -11,21 +14,14 @@ SIZE = 1024
 BSIZE = 1024 * 4
 S = ' '
 
-def sym_encr():
-    return 0
-    #FALTA
-
 def main(encrypt_opt):
     #CONEXION TPC 
+    
     ip = socket.gethostbyname(socket.gethostname())
     port = 2222
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.connect((ip,port))
-
-    #MODO DE INCRIPTACION
-    if(encrypt_opt == 's'):
-        encr_pass = input("Enter password: ")
 
     #IDENTIFICACION DE DATOS
     root = tk.Tk()                                  #Obtener root
@@ -34,15 +30,37 @@ def main(encrypt_opt):
     file_name = os.path.basename(filepath)
     file_size = os.path.getsize(filepath)
 
+     #MODO DE INCRIPTACION
+    if(encrypt_opt == 's'):
+        #encr_pass = bytes(input("Enter password: ") ,FORM)
+        #with open('py_pass.txt', "wb") as p:
+            #p.write(encr_pass)
+        encr_pass = b'mysecretpassword'
+        cipher = AES.new(encr_pass, AES.MODE_CBC)
+        with open(filepath, "rb") as f:
+            orig_file = f.read()
+            padded_file = pad(orig_file,AES.block_size)    
+            encrypted_message = cipher.iv + cipher.encrypt(padded_file)
+            file_size = len(encrypted_message)
+            
+    
     #FORMATO DE ENVIO: "nombre_del_archivo 'S' tamaño_del_archivo 'S' forma_de_incriptacion "
+    print(f"{file_name}{S}{file_size}{S}{encrypt_opt}")
     server.send(f"{file_name}{S}{file_size}{S}{encrypt_opt}".encode(FORM))
     msg = server.recv(SIZE).decode(FORM)
     print(msg)
 
-    progress = tqdm.tqdm(range(file_size), f"Sending {file_name}", unit="B", unit_scale=True, unit_divisor=1024) #actualizacion de la barra de progreso 
+    progress = tqdm.tqdm(range(file_size), 
+                         f"Sending {file_name}", 
+                         unit="B", unit_scale=True, 
+                         unit_divisor=1024) #actualizacion de la barra de progreso 
+
     with open(filepath, "rb") as file:
         while True:
-            data = file.read(BSIZE)                 #se envian los datos un buffer a la vez
+            if(encrypt_opt == 's'):
+                data = encrypted_message(BSIZE)  
+            else: 
+                data = file.read(BSIZE)                 #se envian los datos un buffer a la vez
             if not data:
                 break
 
