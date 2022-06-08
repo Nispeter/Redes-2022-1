e5b70f3250b9d3cda4ec5be3f975b918ff948218
Didozenny <71187727+Didozenny@users.noreply.github.com>
2022-06-08 19:21:28 -0400

    Add files via upload

---
+++ client_server_SyA_encryption/asymetric_key_generation.py
@@ -0,0 +1,29 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+private_key = rsa.generate_private_key(
+
+    public_exponent=65537,
+
+    key_size=2048,
+
+)
+
+public_key = private_key.public_key()
+
+pem = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption()
+)
+
+with open('private_key.pem', 'wb') as f:
+    f.write(pem)
+
+publicPem = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+with open('public_key.pem', 'wb') as f:
+    f.write(publicPem)
