4e2bb6c6e02fedc4847f13d711cd170600057766
Nispeter <Nispeter2@gmail.com>
2022-06-08 19:48:47 -0400

    run.py for asym

--- TCP_client_server/createSalt.py
+++
@@ -1,9 +0,0 @@
-from base64 import b64encode, b64decode
-import hashlib
-from Crypto.Cipher import AES
-import os
-from Crypto.Random import get_random_bytes
-
-salt=get_random_bytes(AES.block_size)
-
-print(salt)
