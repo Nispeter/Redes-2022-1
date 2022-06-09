86cc9583582822041b9b61401c833e302162f4d7
Didozenny <71187727+Didozenny@users.noreply.github.com>
2022-06-08 23:43:01 -0400

    Update server_test.py

--- client_server_SyA_encryption/server/server_test.py
+++ client_server_SyA_encryption/server/server_test.py
@@ -12,7 +12,7 @@ FORM = "utf-8"
 SIZE = 1024
 BSIZE = 1024 * 4
 S = ' '
-password = 'sexo'
+password = 'clave'
 salt = b'\xe8\xc7BD2\x0e\x12u<\xc9\xee\xa7f\x9cO\xbf'
 
 def key_generation(password, salt):#creamos key en base a una clave y salt
@@ -75,22 +75,21 @@ def main():
         print(f"Connection Established - {address[0]}:{address[1]}")
 
         head = client.recv(BSIZE).decode()
-        file_name, file_size,encrypt_opt = head.split(S)  
+        file_name, file_size,encrypt_opt = head.split(S)
+        original_name = file_name
         client.send(('File size & name received').encode(FORM))
 
         if (encrypt_opt == 's'): 
             encr_print = 'sym_deencr'
-            original_name = file_name
             file_name = 'encr_' + file_name
 
         elif(encrypt_opt == 'a'):
             encr_print = 'asym_deencr'
-            original_name = file_name
             file_name = 'encr_' + file_name
 
         if (encrypt_opt == 'o'): encr_print = 'none'
 
-        print("File name received: ", file_name, "\nFile size received: ", file_size,"\nEncryption Option: ", encr_print)
+        print("File name received: ", original_name, "\nFile size received: ", file_size,"\nEncryption Option: ", encr_print)
         file_size = int(file_size)
 
         progress = tqdm.tqdm(range(file_size), f"Receiving {file_name}", unit="B", unit_scale=True, unit_divisor=1024)
@@ -98,7 +97,7 @@ def main():
         with open(file_name, "wb") as file:
             while True:
                 data = client.recv(BSIZE)
-                if not data:    
+                if not data:
                     break
                 file.write(data)
                 progress.update(len(data))
